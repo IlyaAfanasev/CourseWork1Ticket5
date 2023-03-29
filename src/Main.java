14f0e4f580e6996568cf157cbf1031523055ab7b
IlyuAfanasev <gecktor.86@mail.ru>
2023-03-29 16:57:40 +0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] numbers= {1,2,3,4,5,6,7,8,9,10};
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                numbers[i]+=1;
+            }
+        }
+        System.out.println(Arrays.toString(numbers));
+        for (int i = 2; i <7 ; i++) {
+            System.out.println(numbers[i]);
+
+        }
+        int [] numbersCopy= Arrays.copyOfRange(numbers,2,7);
+        System.out.println(Arrays.toString(numbersCopy));
     }
 }
